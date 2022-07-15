367ceddd5bdb8c800691c864a567f349a22f4f12
Santiago Salgado <jssalgadog@gmail.com>
2022-07-15 05:42:41 -0500

    class_ventajas_programacion_poo

--- main.js
+++ main.js
@@ -1,59 +1,124 @@
-const natalia = {
-    name: "Natalia",
-    age: 20,
-    cursosAprobados: [
-        "Curso Definitivo de HTML y CSS",
-        "Curso Práctico de HTML y CSS"
-    ],
-    aprobarCurso(nuevoCurso) {
-        this.cursosAprobados.push(nuevoCurso);
+// const juan1 = {
+//     name: "JuanDC",
+//     username: "juandc",
+//     points: 100,
+//     socialMedia: {
+//         twitter: "fjuandc",
+//         instagram: "fjuandc",
+//         facebook: undefined
+//     },
+//     approvedCourses: [
+//         "Curso Definitivo de HTML y CSS",
+//         "Curso Práctico de HTML y CSS"
+//     ],
+//     learningPaths: [
+//         {
+//             name: "Escuela de Desarrollo Web",
+//             courses: [
+//                 "Curso Definitivo de HTML y CSS",
+//                 "Curso Practico de HTML y CSS"
+//             ]
+//         },
+//         {
+//             name: "Escuela de Videojuegos",
+//             courses: [
+//                 "Curso Definitivo de HTML y CSS",
+//                 "Curso Practico de HTML y CSS"
+//             ]
+//         },
+//     ],
+// };
+
+// const miguelito1 = {
+//     name: "Miguelito1",
+//     username: "miguelitofeliz",
+//     points: 1000,
+//     socialMedia: {
+//         twitter: "miguelitofeliz",
+//         instagram: "miguelito_feliz",
+//         facebook: undefined
+//     },
+//     approvedCourses: [
+//         "Curso DataBusiness",
+//         "Curso DataViz"
+//     ],
+//     learningPaths: [
+//         {
+//             name: "Escuela de Desarrollo Web",
+//             courses: [
+//                 "Curso Definitivo de HTML y CSS",
+//                 "Curso Practico de HTML y CSS"
+//             ]
+//         },
+//         {
+//             name: "Escuela de Data Science",
+//             courses: [
+//                 "Curso DataBusiness",
+//                 "Curso DataViz"
+//             ]
+//         },
+//     ],
+// };
+
+
+class Course {
+    constructor({
+      id,
+      name,
+      teacher,
+      lessons = [],
+    }) {
+      this.id = id;
+      this.name = name;
+      this.teacher = teacher;
+      this.lessons = lessons;
     }
-}; // Objeto literal
-
-
-function Student(name, age, cursosAprobados) {
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-    // this.aprobarCurso = function (nuevoCurso) {
-    //     this.cursosAprobados.push(nuevoCurso)
-    // }
 }
 
-Student.prototype.aprobarCurso = function (nuevoCurso) {
-        this.cursosAprobados.push(nuevoCurso)
-    }
-
-const juanita = new Student(
-    "Juanita", 
-    19, 
-    ["Curso de Introducción a la Producción de Videojuegos", 
-    "Curso de Creacion de personajes"]);
-// Hacer que Natalia apruebe otro curso
-natalia.cursosAprobados.push("Curso de Responsive Design");
-
-// Prototipos con la sintaxis de clases
-class Student2 {
+class learningPath {
     constructor ({
-            email,
-            name, 
-            age, 
-            cursosAprobados = [],
+            name,
+            courses = [],
         }) {
         this.name = name;
-        this.age = age;
-        this.cursosAprobados = cursosAprobados;
-        this.email = email;
-    }
-
-    aprobarCurso(nuevoCurso) {
-        this.cursosAprobados.push(nuevoCurso)
+        this.courses = courses;
     }
 }
 
-const miguelito = new Student2(
-    { 
-        email: "migelito@email.com",
-        name: "Miguelito", 
-        age: 28
-    })
+class Student {
+    constructor ({
+        name, 
+        email,
+        username,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = []
+    }) {
+        this.name = name;
+        this.email = email;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook
+        };
+        this.username = username;
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
+    }
+}
+
+const juan2 = new Student({
+    name: "juanDc",
+    username:"JuanDE",
+    email:"juan@email.com",
+    twitter: "JuanDC"
+});
+
+const miguelito1 = new Student({
+    name: "miguelitofeliz",
+    username:"miguelitofeliz",
+    email:"miguelitofeliz@email.com",
+    twitter: "miguelitofeliz"
+});
