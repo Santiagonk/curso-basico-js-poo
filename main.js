b5b5a98c4ebd804804522e9fc767274a6317aeb2
Santiago Salgado <jssalgadog@gmail.com>
2022-07-17 10:36:08 -0500

    class_abstraccion

--- main.js
+++ main.js
@@ -1,81 +1,14 @@
-// const juan1 = {
-//     name: "JuanDC",
-//     username: "juandc",
-//     points: 100,
-//     socialMedia: {
-//         twitter: "fjuandc",
-//         instagram: "fjuandc",
-//         facebook: undefined
-//     },
-//     approvedCourses: [
-//         "Curso Definitivo de HTML y CSS",
-//         "Curso Práctico de HTML y CSS"
-//     ],
-//     learningPaths: [
-//         {
-//             name: "Escuela de Desarrollo Web",
-//             courses: [
-//                 "Curso Definitivo de HTML y CSS",
-//                 "Curso Practico de HTML y CSS"
-//             ]
-//         },
-//         {
-//             name: "Escuela de Videojuegos",
-//             courses: [
-//                 "Curso Definitivo de HTML y CSS",
-//                 "Curso Practico de HTML y CSS"
-//             ]
-//         },
-//     ],
-// };
-
-// const miguelito1 = {
-//     name: "Miguelito1",
-//     username: "miguelitofeliz",
-//     points: 1000,
-//     socialMedia: {
-//         twitter: "miguelitofeliz",
-//         instagram: "miguelito_feliz",
-//         facebook: undefined
-//     },
-//     approvedCourses: [
-//         "Curso DataBusiness",
-//         "Curso DataViz"
-//     ],
-//     learningPaths: [
-//         {
-//             name: "Escuela de Desarrollo Web",
-//             courses: [
-//                 "Curso Definitivo de HTML y CSS",
-//                 "Curso Practico de HTML y CSS"
-//             ]
-//         },
-//         {
-//             name: "Escuela de Data Science",
-//             courses: [
-//                 "Curso DataBusiness",
-//                 "Curso DataViz"
-//             ]
-//         },
-//     ],
-// };
-
-
 class Course {
     constructor({
-      id,
       name,
-      teacher,
       lessons = [],
     }) {
-      this.id = id;
       this.name = name;
-      this.teacher = teacher;
       this.lessons = lessons;
     }
 }
 
-class learningPath {
+class LearningPath {
     constructor ({
             name,
             courses = [],
@@ -109,16 +42,79 @@ class Student {
     }
 }
 
+const cursoProgBasica = new Course ({
+    name: "Curso de Programación Básica"
+});
+
+const definitivoHtml = new Course ({
+    name: "Curso de Programación Básica"
+});
+
+const practicoHtml = new Course ({
+    name: "Curso Práctico de HTML y CSS"
+});
+
+const businessIntelligence = new Course ({
+    name: "Curso BI"
+});
+
+const dataViz = new Course ({
+    name: "Curso DataViz"
+});
+
+const unity = new Course ({
+    name: "Curso de Unity"
+});
+
+const unreal = new Course ({
+    name: "Curso de Unreal"
+});
+
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrolo Web",
+    courses: [
+        cursoProgBasica,
+        definitivoHtml,
+        practicoHtml
+    ]
+});
+
+const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+        businessIntelligence,
+        dataViz
+    ]
+});
+
+const escuelaVgs = new LearningPath({
+    name: "Escuela de Videojuegos",
+    courses: [
+        unity,
+        unreal
+    ]
+});
+
+
+
+
 const juan2 = new Student({
     name: "juanDc",
     username:"JuanDE",
     email:"juan@email.com",
-    twitter: "JuanDC"
+    twitter: "JuanDC",
+    learningPaths: [
+        escuelaVgs,
+        escuelaWeb
+    ]
 });
 
 const miguelito1 = new Student({
     name: "miguelitofeliz",
     username:"miguelitofeliz",
     email:"miguelitofeliz@email.com",
-    twitter: "miguelitofeliz"
+    twitter: "miguelitofeliz",
+    learningPaths: [
+        escuelaData
+    ]
 });
