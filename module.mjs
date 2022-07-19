8c6afedf11e566e579fd07ee600e859fc8ca3f29
Santiago Salgado <jssalgadog@gmail.com>
2022-07-19 05:28:21 -0500

    class_ecmascript6

---
+++ module.mjs
@@ -0,0 +1,21 @@
+import {PlatziClass} from "./main.mjs";
+
+const clase67 = new PlatziClass({
+    name: "JavaScript Orientado a Objetos Basado en Prototipos",
+    videoID: "dfasdfasdf-asdfasdf-adfasd"
+});
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
+
+clase67.reproducir();
+clase67.pausar();
