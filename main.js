ce7c94643e77bcf5105df0040734cf61311295bd
Santiago Salgado <jssalgadog@gmail.com>
2022-08-08 05:34:54 -0500

    Class_shallow_copy

--- main.js
+++ main.js
@@ -1,51 +1,17 @@
-const juan = {
-    name: "Juanito",
-    age: 18,
-    approvedCourses: ["Curso 1"],
-    addCourse(newCourse) {
-        console.log(" This", this);
-        console.log(" This.approvedCourses", this.approvedCourses);
-        this.approvedCourses.push(newCourse);
+const obj1 = {
+    a: 'a',
+    b: 'b',
+    c: {
+        d: "d",
+        e: "e"
     }
 };
 
+const obj2 = {};
 
-Object.defineProperty(juan, "",{
-    value:"",
-    writable:true,
-    enumerable:true,
-    configurable:true
-});
+for (prop in obj1){
+    obj2[prop] = obj1[prop]
+}
 
-Object.defineProperty(juan, "PruebaNasa",{
-    value:"Estraterrestres",
-    writable:false,
-    enumerable:false,
-    configurable:false
-});
-
-Object.defineProperty(juan, "navigator",{
-    value:"Chrome",
-    writable:true,
-    enumerable:false,
-    configurable:true
-});
-
-Object.defineProperty(juan, "editor",{
-    value:"VSCode",
-    writable:false,
-    enumerable:true,
-    configurable:true
-});
-
-
-Object.defineProperty(juan, "terminal",{
-    value:"WSL",
-    writable:true,
-    enumerable:true,
-    configurable:false
-});
-
-Object.seal(juan);
-Object.freeze(juan);
-console.log(Object.getOwnPropertyDescriptors(juan));
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
