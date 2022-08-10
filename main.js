edd7cc3f88f415f6b887d7c435b415cd4f1c352a
Santiago Salgado <jssalgadog@gmail.com>
2022-08-10 06:39:23 -0500

    class_recursividad

--- main.js
+++ main.js
@@ -10,5 +10,24 @@ const obj1 = {
     }
 };
 
-const stringifiedComplexObj = JSON.stringify(obj1);
-const obj2 = JSON.parse(stringifiedComplexObj);
+// const stringifiedComplexObj = JSON.stringify(obj1);
+// const obj2 = JSON.parse(stringifiedComplexObj);
+
+const numeritos = [0,1,3,4,5,6,7,3,1,3,5,7,6,7];
+// let numerito = 0;
+// for (let index = 0; index < numeritos.length; index++) {
+//     numerito = numeritos[index]
+//     console.log({index, numerito});
+// }
+
+function recursiva(numbersArray, index) {
+    if (numbersArray.length != 0) {
+        const number = numbersArray[0];
+        numbersArray.shift()
+        console.log({index, number});
+        index++;
+        return recursiva(numbersArray, index);
+    } 
+}
+
+recursiva(numeritos, 0);
