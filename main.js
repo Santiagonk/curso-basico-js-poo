eaa78478a6629732065675b2f2a89eb806555417
Santiago Salgado <jssalgadog@gmail.com>
2022-08-18 05:28:25 -0500

    class_getters_setters2

--- main.js
+++ main.js
@@ -87,22 +87,32 @@ function createStudent({
         },
         approvedCourses,
         learningPaths,
-        readName() {
+        get name(){
             return private._name;
         },
-        changeName(newName) {        
-            private._name = newName;        
-        }
+        set name(newName){
+            if(newName.length != 0){
+                private._name = newName; 
+            } else {
+                console.warn("Tu nombre debe tener al menos 1 caracter");
+            }       
+        }        
+        // readName() {
+        //     return private._name;
+        // },
+        // changeName(newName) {        
+        //     private._name = newName;        
+        // }
     };
     
-    Object.defineProperty(public, "readName", {
-        configurable: false,
-        writable: false
-    });
-    Object.defineProperty(public, "changeName", {
-        configurable: false,
-        writable: false
-    });
+    // Object.defineProperty(public, "readName", {
+    //     configurable: false,
+    //     writable: false
+    // });
+    // Object.defineProperty(public, "changeName", {
+    //     configurable: false,
+    //     writable: false
+    // });
     return public;
 }
 
